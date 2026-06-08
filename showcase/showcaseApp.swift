8234439fcb9b01309277943a1437aa8111c58b9c
rafaelasenciodev <rafaelasenciob@icloud.com>
2026-06-08 16:51:12 +0200

    Initial Commit

---
+++ showcase/showcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  showcaseApp.swift
+//  showcase
+//
+//  Created by Rafa Asencio on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct showcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ showcaseTests/showcaseTests.swift
@@ -0,0 +1,18 @@
+//
+//  showcaseTests.swift
+//  showcaseTests
+//
+//  Created by Rafa Asencio on 08/06/2026.
+//
+
+import Testing
+
+struct showcaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
